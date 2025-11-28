b029856873c8ee233ac25a92fafae36579161cda
techsynergysolutions2-sys <techsynergysolutions2@gmail.com>
2025-11-29 00:20:11 +0200

    Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,97 +11,19 @@ import SidebarMenu from '../components/SidebarMenu';
 
 const { Header, Content } = Layout;
 
-const items = [
-    {
-      key: '1',
-      label: (
-        <a href="/crm">
-          CRM
-        </a>
-      ),
-    },
-    {
-      key: '2',
-      label: (
-        <a href="/projects">
-          Projects
-        </a>
-      ),
-    },
-    {
-      key: '3',
-      label: (
-        <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-          Sales
-        </a>
-      ),
-    },
-    {
-      key: '4',
-      label: (
-        <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-          TimeSheet
-        </a>
-      ),
-    },
-    {
-        key: '5',
-        label: (
-          <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-            Clients
-          </a>
-        ),
-      },
-  ];
-
 function Home() {
 
   const navigate = useNavigate();
-  const [userInfor, setUserInfor] = useState(null)
 
   useEffect(() => {
-fnCheckExpiryDate()
-    // console.log('************************')
-    // console.log(fnLogin())
-    // console.log('***************************')
-    // if(fnLogin() == false){
-    //   // window.location.replace("http://localhost:3000/login");
-    // }
+    fnCheckExpiryDate()
 
-    const fnGetUserInfor = () => {
-      onAuthStateChanged(auth,(user) => {
-        if (user) {
-          console.log(user)
-          setUserInfor(user)
-        } else {
-          navigate('/login')
-          // User is signed out
-          // ...
-        }
-      });
-    }
-
-    fnGetUserInfor()
-    console.log(userInfor)
   },[])
 
   return (
  <Layout style={{height: '100vh'}}>
     <SidebarMenu collapse={false} />
     <Layout>
-         {/* <Header style={{backgroundColor: '#fff',boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)' ,height: 50, paddingLeft: '8px', paddingRight: '8px', position: 'sticky', top: 0, zIndex: 500}}>
-             <Row style={{ flex: 1}}>
-                 <Col span={24}><Typography style={{...Styles,float: 'right', marginRight: 5, fontWeight: 500, color: 'black', marginTop: 5}}>
-                     <Badge count={2} style={{marginRight: '40px',fontSize:'12px', marginTop: '3px'}} color='green'>
-                         <MessageOutlined style={{fontSize: '20px', color: 'black',marginRight: '40px'}}/>
-                     </Badge>
-                     <Badge count={5} style={{marginRight: '40px',fontSize:'12px', marginTop: '3px'}} color='green'>
-                         <BellOutlined style={{fontSize: '20px', color: 'black',marginRight: '40px'}}/>
-                     </Badge>
-                     {userInfor?.displayName} <Avatar size='large' icon={<UserOutlined />} /></Typography>
-                 </Col>
-             </Row>
-         </Header> */}
 
          <Content>
             
